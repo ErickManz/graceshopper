6e20400a58143de0fd62f8622f2315af621e1ee5
Alberto Huerta <albertohuerta@AlbertoHuertaMac.local>
2022-03-01 17:03:27 -0500

    finished and tested  meme model and get all route for meme

---
+++ server/api/memes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const {
+  models: { Meme },
+} = require('../db');
+module.exports = router;
+
+router.get('/', async (req, res, next) => {
+  try {
+    const memes = await Meme.findAll();
+    res.json(memes);
+  } catch (err) {
+    next(err);
+  }
+});
