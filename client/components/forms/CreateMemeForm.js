eaf881455f70c5d6753b0fb6c103fb8e45f6b766
Colin Forbes <cjforbes32@gmail.com>
2022-03-03 12:34:38 -0500

    forgot to save files for previous commit

---
+++ client/components/forms/CreateMemeForm.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'React';
+import { useDispatch } from 'react-redux';
+
+function CreateMemeForm(props) {}
--- client/store/index.js
+++ client/store/index.js
@@ -4,9 +4,13 @@ import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import auth from './auth';
 import memesReducer from './allMemes';
-import singleMemeReducer from './SingleMemes';
+import singleMemeReducer from './singleMemes';
 
-const reducer = combineReducers({ auth, memes: memesReducer , singleMeme: singleMemeReducer});
+const reducer = combineReducers({
+  auth,
+  memes: memesReducer,
+  singleMeme: singleMemeReducer,
+});
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
@@ -14,4 +18,3 @@ const store = createStore(reducer, middleware);
 
 export default store;
 export * from './auth';
-
