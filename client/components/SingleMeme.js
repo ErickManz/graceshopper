8eaf1a1d5c2f398427575a97a4f3db63f145a88c
Colin Forbes <cjforbes32@gmail.com>
2022-03-03 18:05:45 -0500

    made some small changes to singleMeme redux store file to enhance readability

--- client/components/SingleMeme.js
+++ client/components/SingleMeme.js
@@ -1,43 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getSingleMeme } from '../store/SingleMemes';
+import { getMeme } from '../store/singleMeme';
 import { addItems } from '../store/cart';
-import {me} from '../store';
-
-
+import { me } from '../store';
 
 function SingleMeme(props) {
-  const meme = useSelector(state => state.singleMeme)
-  const user = useSelector(state => state.auth.id);
+  const meme = useSelector((state) => state.singleMeme);
+  const user = useSelector((state) => state.auth.id);
   const [quantity, setQuantity] = useState(1);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getSingleMeme(props.match.params.id));
+    dispatch(getMeme(props.match.params.id));
     dispatch(me());
   }, []);
-  const onSubmit = (e ,memeId) =>{
+  const onSubmit = (e, memeId) => {
     e.preventDefault();
-    dispatch(addItems(user,{memeId: memeId, quantity: quantity}))
+    dispatch(addItems(user, { memeId: memeId, quantity: quantity }));
     setQuantity(1);
-  }
+  };
 
   return (
-          <div key={meme.id} className="singleMeme">
-            <h2>{meme.name}</h2>
-            <h4>{meme.price}</h4>
-            <h4>{meme.genre}</h4>
-            <img src={meme.imageUrl} />
-            <p>{`${meme.description} by ${meme.artist}`}</p>
-            <h4>Stock quantity left: {meme.stockQuantity}</h4>
-            <div>
-            <label htmlFor="quantity">Quantity:</label>
-            <input type= "number" name="quantity" value={quantity} onChange={e=> setQuantity(e.target.value)} />
-
-            </div>
-            <button type= "button" onClick={(e) => onSubmit(e,meme.id)} >Add to cart </button>
-          </div>
-
+    <div key={meme.id} className="singleMeme">
+      <h2>{meme.name}</h2>
+      <h4>{meme.price}</h4>
+      <h4>{meme.genre}</h4>
+      <img src={meme.imageUrl} />
+      <p>{`${meme.description} by ${meme.artist}`}</p>
+      <h4>Stock quantity left: {meme.stockQuantity}</h4>
+      <div>
+        <label htmlFor="quantity">Quantity:</label>
+        <input
+          type="number"
+          name="quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+      </div>
+      <button type="button" onClick={(e) => onSubmit(e, meme.id)}>
+        Add to cart{' '}
+      </button>
+    </div>
   );
 }
 
