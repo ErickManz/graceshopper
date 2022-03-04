de987799f4be5c5e758e734eda0cde0d58d415f6
Alberto Huerta <albertohuerta@AlbertoHuertaMac.local>
2022-03-03 19:12:52 -0500

    restructured cart with hooks

--- client/Routes.js
+++ client/Routes.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment, useEffect } from 'react';
-import { connect, useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 import AllMemes from './components/AllMemes';
 import SingleMemes from './components/SingleMeme';
-import Confirmation from './components/Confirmation'
-import Checkout from './components/Checkout'
+import Confirmation from './components/Confirmation';
+import Checkout from './components/Checkout';
+import Cart from './components/Cart';
 import { me } from './store';
 
 /**
@@ -26,18 +27,21 @@ const Routes = () => {
       {isLoggedIn ? (
         <Switch>
           <Route path="/home" component={Home} />
-          <Route exact path="/memes" component={AllMemes}/>
-          <Route path="/memes/:id" component={SingleMemes}/>
-          <Route path="/memes" component={AllMemes}/>
-          <Route path="/confirmation" component={Confirmation}/>
-          <Route path="/checkout" component={Checkout}/>
+          <Route exact path="/memes" component={AllMemes} />
+          <Route path="/memes/:id" component={SingleMemes} />
+          <Route path="/memes" component={AllMemes} />
+          <Route path="/confirmation" component={Confirmation} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/mycart" component={Cart} />
           <Redirect to="/home" />
         </Switch>
       ) : (
         <Switch>
-          <Route path="/confirmation" component={Confirmation}/>
-          <Route path="/checkout" component={Checkout}/>
-          <Route path="/" exact>{Login}</Route>
+          <Route path="/confirmation" component={Confirmation} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" exact>
+            {Login}
+          </Route>
           <Route path="/login">{Login}</Route>
           <Route path="/signup">{Signup}</Route>
         </Switch>
--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,35 +1,48 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMemes } from '../store/allMemes';
+import { Link } from 'react-router-dom';
 import { fetchItems } from '../store/cart';
 
-export class Cart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      item: '',
-    };
-  }
+function Cart(props) {
+  const cartItems = useSelector((state) => state.cartItems);
+  const dispatch = useDispatch();
 
-  componentDidMount() {
-    fetchItems();
-  }
+  useEffect(() => {
+    dispatch(fetchItems());
+  }, []);
 
-  render() {
-    console.log(this.props);
-    return <div></div>;
-  }
+  console.log(props);
+  return (
+    <div>
+      {/* <div key={meme.id} className="singleMeme">
+        <h2>{meme.name}</h2>
+        <h4>{meme.price}</h4>
+        <h4>{meme.genre}</h4>
+        <img src={meme.imageUrl} />
+        <p>{`${meme.description} by ${meme.artist}`}</p>
+        <h4>{meme.stockQuantity}</h4>
+        <button type="button">
+          {' '}
+          <Link to="/checkout">Checkout </Link>
+        </button>
+      </div> */}
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    item: state.item,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     item: state.item,
+//   };
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchItems: () => dispatch(fetchItems()),
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     fetchItems: () => dispatch(fetchItems()),
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cart);
+// export default connect(mapStateToProps, mapDispatchToProps)(Cart);
+
+export default Cart;
