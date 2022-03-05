d26375389c1802524726e94e8f1be31d947bd148
ErickManz <erickmanzanares22@gmail.com>
2022-03-05 13:52:33 -0500

    redux store now can get singel users

--- client/store/User.js
+++ client/store/User.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
 const GET_USER = "GET_USER"
+const GET_SINGLE_USER ="GET_SINGLE_USER"
 
-const AllUser = (user) =>({
+const AllUser = (users) =>({
   type:GET_USER,
+  users
+})
+
+const SingleUser =(user) => ({
+  type:GET_SINGLE_USER,
   user
 })
 
-export const getUser = () =>{
+export const getUsers = () =>{
   return async (dispatch) =>{
     try {
       const {data} = await axios.get('/api/users')
@@ -17,9 +23,21 @@ export const getUser = () =>{
     }
   }
 }
+export const getUser = (id) => {
+  return async (dispatch) =>{
+    try{
+      const {data} =await axios.get(`api/users/${id}`);
+      dispatch(SingleUser(data));
+    }catch(error){
+      console.error(error);
+    }
+  }
+}
 const userReducer = (user = [], action) =>{
   switch(action.type){
     case GET_USER:
+      return action.users
+    case GET_SINGLE_USER:
       return action.user
     default:
       return user;
