b5ae6a0cc41b91fe96eefe03587b3159dbf87b14
ErickManz <erickmanzanares22@gmail.com>
2022-03-05 22:49:10 -0500

    add to the singleUser store a axios call to edit user

--- client/store/SingleUser.js
+++ client/store/SingleUser.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 const GET_SINGLE_USER ="GET_SINGLE_USER"
+const UPDATE_USER = "UPDATE_USER"
 
 const SingleUser =(user) => ({
   type:GET_SINGLE_USER,
+  user
+})
+
+const updateUser = (user) =>({
+  type:UPDATE_USER,
   user
 })
 
@@ -17,9 +23,21 @@ export const getUser = (id) => {
     }
   }
 }
+export const editUser = (id, info) => {
+  return async (dispatch) =>{
+    try{
+      const {data} =await axios.get(`/api/users/${id}/update`, info);
+      dispatch(updateUser(data));
+    }catch(error){
+      console.error(error);
+    }
+  }
+}
 const singleUserReducer = (state = {}, action) =>{
   switch(action.type){
     case GET_SINGLE_USER:
+      return action.user
+    case UPDATE_USER:
       return action.user
     default:
       return state;
