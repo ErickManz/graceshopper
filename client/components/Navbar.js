32901a35fdb12bb775dc4354ec36a35b7b5769f3
Colin Forbes <cjforbes32@gmail.com>
2022-03-07 11:45:39 -0500

    renamed app to MemeSupreme

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,20 +2,20 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
-import {me} from '../store';
+import { me } from '../store';
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.id);
   const isLogged = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   console.log(isLogged);
- useEffect(()=>{
-   dispatch(me());
- },[])
+  useEffect(() => {
+    dispatch(me());
+  }, []);
   return (
     <div>
-      <h1>memeMart</h1>
+      <h1>MemeSupreme</h1>
       <nav>
-        {isLoggedIn && (isLogged.roleId === 1) ? (
+        {isLoggedIn && isLogged.roleId === 1 ? (
           <div>
             {/* The navbar will show these links after you log in and you are a admin */}
             <Link to="/home">Home</Link>
@@ -26,27 +26,25 @@ const Navbar = () => {
             <Link to="/mycart">View My Cart</Link>
             <Link to="/createMeme">Create Meme</Link>
             <Link to="/users">View all users</Link>
-
           </div>
-        ) : isLoggedIn && (isLogged.roleId === 2) ? (
+        ) : isLoggedIn && isLogged.roleId === 2 ? (
           <div>
-          {/* The navbar will show these links after you log in and you are a users */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={() => dispatch(logout())}>
-            Logout
-          </a>
-          <Link to="/memes">All Memes</Link>
-          <Link to="/mycart">View My Cart</Link>
-
-        </div>
-      ) :(
+            {/* The navbar will show these links after you log in and you are a users */}
+            <Link to="/home">Home</Link>
+            <a href="#" onClick={() => dispatch(logout())}>
+              Logout
+            </a>
+            <Link to="/memes">All Memes</Link>
+            <Link to="/mycart">View My Cart</Link>
+          </div>
+        ) : (
           <div>
             {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
             <a href="#" onClick={() => dispatch(logout())}>
-            Logout
-          </a>
+              Logout
+            </a>
 
             <Link to="/memes">All Memes</Link>
           </div>
