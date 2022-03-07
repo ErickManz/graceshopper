8d436755d747c792fded6f5dcc299357b67dbd88
ErickManz <erickmanzanares22@gmail.com>
2022-03-07 18:56:09 -0500

    when siging up user now have a role id of 2 which is user"

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../db');
+const { User,Role } = require('../db');
 module.exports = router;
 
 router.post('/login', async (req, res, next) => {
@@ -13,6 +13,7 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body);
+    user.setRole(2); // new user are auto user
     res.send({ token: await user.generateToken() });
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
