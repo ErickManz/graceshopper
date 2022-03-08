0fc5073357544afb4eaf1fd9346916832ebf2ad8
Colin Forbes <cjforbes32@gmail.com>
2022-03-08 13:31:26 -0500

    finished coding the skeleton of the adress portion of the signup form. functionality forthcoming

--- client/components/forms/AuthForm.js
+++ client/components/forms/AuthForm.js
@@ -93,6 +93,85 @@ const AuthForm = ({ name, displayName }) => {
                 <small>City</small>
               </label>
               <input name="city" placeholder="City" required={true} />
+
+              <label htmlFor="State">
+                <small>State</small>
+              </label>
+              <select name="state">
+                <option value="AL">Alabama</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="CA">California</option>
+                <option value="CO">Colorado</option>
+                <option value="CT">Connecticut</option>
+                <option value="DE">Delaware</option>
+                <option value="DC">District Of Columbia</option>
+                <option value="FL">Florida</option>
+                <option value="GA">Georgia</option>
+                <option value="HI">Hawaii</option>
+                <option value="ID">Idaho</option>
+                <option value="IL">Illinois</option>
+                <option value="IN">Indiana</option>
+                <option value="IA">Iowa</option>
+                <option value="KS">Kansas</option>
+                <option value="KY">Kentucky</option>
+                <option value="LA">Louisiana</option>
+                <option value="ME">Maine</option>
+                <option value="MD">Maryland</option>
+                <option value="MA">Massachusetts</option>
+                <option value="MI">Michigan</option>
+                <option value="MN">Minnesota</option>
+                <option value="MS">Mississippi</option>
+                <option value="MO">Missouri</option>
+                <option value="MT">Montana</option>
+                <option value="NE">Nebraska</option>
+                <option value="NV">Nevada</option>
+                <option value="NH">New Hampshire</option>
+                <option value="NJ">New Jersey</option>
+                <option value="NM">New Mexico</option>
+                <option value="NY">New York</option>
+                <option value="NC">North Carolina</option>
+                <option value="ND">North Dakota</option>
+                <option value="OH">Ohio</option>
+                <option value="OK">Oklahoma</option>
+                <option value="OR">Oregon</option>
+                <option value="PA">Pennsylvania</option>
+                <option value="RI">Rhode Island</option>
+                <option value="SC">South Carolina</option>
+                <option value="SD">South Dakota</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
+              </select>
+
+              <label htmlFor="zip">
+                <small>ZIP Code</small>
+              </label>
+              <input
+                name="zip"
+                type="number"
+                placeholder="12345"
+                required={true}
+              />
+
+              <label htmlFor="phone">
+                <small>Telephone XXX-XXX-XXXX</small>
+              </label>
+              <input
+                name="phone"
+                type="tel"
+                required={true}
+                required
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                placeholder="ex. 123-456-7890"
+              />
             </>
           ) : null}
         </div>
