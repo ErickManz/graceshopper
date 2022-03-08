6547530bb6c67d7ba33e9d37e390deebd487677b
ErickManz <erickmanzanares22@gmail.com>
2022-03-07 23:41:10 -0500

    now user can no longerput empty string to make an account

--- client/components/forms/AuthForm.js
+++ client/components/forms/AuthForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { authenticate } from '../../store';
 
@@ -8,15 +8,26 @@ import { authenticate } from '../../store';
 const AuthForm = ({ name, displayName }) => {
   const { error } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const [username, setUsername] = useState('');
+  const [password,setPassword] = useState('');
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const formName = evt.target.name;
-    const username = evt.target.username.value;
-    const password = evt.target.password.value;
+
     dispatch(authenticate(username, password, formName));
   };
+  const handleChangeForUser= (evt) => {
+    setUsername(evt.target.value);
+  }
+  const handleChangeForPassword= (evt) => {
+    setPassword(evt.target.value);
+  }
 
+  let isUserNamePasswordEmpty = true;
+  if(username.length > 0 && password.length > 0){
+    isUserNamePasswordEmpty = false
+  }
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
@@ -24,16 +35,16 @@ const AuthForm = ({ name, displayName }) => {
           <label htmlFor="username">
             <small>Username</small>
           </label>
-          <input name="username" type="text" />
+          <input name="username" type="text" onChange={handleChangeForUser} />
         </div>
         <div>
           <label htmlFor="password">
             <small>Password</small>
           </label>
-          <input name="password" type="password" />
+          <input name="password" type="password" onChange={handleChangeForPassword}/>
         </div>
         <div>
-          <button type="submit">{displayName}</button>
+          <button type="submit" disabled={isUserNamePasswordEmpty}>{displayName}</button>
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
